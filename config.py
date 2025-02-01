d7813ec1ac6afc594b0d426196b6cbbee3352914
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2025-02-01 22:08:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/cosmic
 
 START_MSG = os.environ.get("START_MESSAGE", "<b><blockquote>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
 try:
-    ADMINS=[1579154183 6950726141]
+    ADMINS=["1579154183 6950726141"]
     for x in (os.environ.get("ADMINS", "1579154183 6950726141").split()):
         ADMINS.append(int(x))
 except ValueError:
