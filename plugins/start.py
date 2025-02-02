9dc85368769fe8a7bc53b9c6450a467e983ebf38
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2025-02-02 13:36:19 +0530

    hi

--- plugins/start.py
+++ plugins/start.py
@@ -117,9 +117,10 @@ async def start_command(client: Client, message: Message):
 
         codeflix_msgs = []
         for msg in messages:
-            caption = (CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, 
-                                             filename=msg.document.Sahiljjng) if bool(CUSTOM_CAPTION) and bool(msg.document)
-                       else ("" if not msg.caption else msg.caption.html))
+            if CUSTOM_CAPTION:
+                    caption = f"{original_caption}\n\n{CUSTOM_CAPTION}"
+                else:
+                    caption = original_caption
 
             reply_markup = msg.reply_markup if DISABLE_CHANNEL_BUTTON else None
 
